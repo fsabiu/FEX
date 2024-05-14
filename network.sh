2677e4925a70c7782e6b3bc70d0ba949c51360a8
fsabiu <fsabiu@gmail.com>
2024-05-14 08:14:35 +0000

    Revert "Revert "Initial commit""
    This reverts commit ffbc5b48d477ef3c72c28e4c46140e7b1b06f8e9.

---
+++ network.sh
@@ -0,0 +1,4 @@
+sudo apt -y install firewalld
+sudo firewall-cmd --zone=public --add-port=2053/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=2054/tcp --permanent
+sudo firewall-cmd --reload
