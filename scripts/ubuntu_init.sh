6b1420ee142fcae3a4a6be9e001aa8dab3004b6b
fsabiu <fsabiu@gmail.com>
2024-05-15 17:54:25 +0000

    gpu requirements

--- scripts/ubuntu_init.sh
+++ scripts/ubuntu_init.sh
@@ -2,7 +2,10 @@ wget https://repo.anaconda.com/archive/Anaconda3-2024.02-1-Linux-x86_64.sh
 chmod +x Anaconda3-2024.02-1-Linux-x86_64.sh
 ./Anaconda3-2024.02-1-Linux-x86_64.sh
 sudo apt-get update && sudo apt-get install libgl1
-apt install alsa-utils
+sudo apt install alsa-utils
 sudo apt install ubuntu-drivers-common
 sudo ubuntu-drivers autoinstall
 sudo apt install unzip
+sudo apt install nvidia-cuda-toolkit
+sudo apt install nvidia-driver-545
+sudo ubuntu-drivers install nvidia:545
