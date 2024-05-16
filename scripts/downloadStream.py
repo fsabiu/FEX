e027d61153bcd5a739a3ba90028e30193ef40dc7
ntefa <andrea.batino94@gmail.com>
2024-05-16 13:46:14 +0000

    updated download stream with control over null stream

--- scripts/downloadStream.py
+++ scripts/downloadStream.py
@@ -2,6 +2,7 @@ import cv2
 import os
 from datetime import datetime
 import sys 
+import time
 
 def open_rtsp_stream(stream_url):
     cap = cv2.VideoCapture(stream_url)
@@ -16,41 +17,49 @@ def release_rtsp_stream(cap):
         cv2.destroyAllWindows()
 
 def save_video_from_stream(stream_url, output_folder, delay):
-    cap = open_rtsp_stream(stream_url)
-    if cap is None:
-        return
 
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     # Create folder if not exists
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
-
-    # Set video resolution
-    cap.set(3, 1280)
-    cap.set(4, 720)
-
-    # Set video codec and get video properties
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-
+        
     while True:
+        
+        cap = open_rtsp_stream(stream_url)
+        if cap is None:
+            print(f"Streaming not active! Retrying in 1 seconds...")
+            time.sleep(1)
+            continue
+
+        # Set video resolution
+        cap.set(3, 1280)
+        cap.set(4, 720)
+
+        # Set video codec and get video properties
+        fps = int(cap.get(cv2.CAP_PROP_FPS))
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    
         # Generate output filename with timestamp
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         output_filename = f"{output_folder}/stream_{timestamp}.mp4"
-        out = cv2.VideoWriter(output_filename, fourcc, fps, (width, height))
 
         start_time = datetime.now()
+        out = cv2.VideoWriter(output_filename, fourcc, fps, (width, height))
+
         while (datetime.now() - start_time).total_seconds() < delay:
+            
             ret, frame = cap.read()
             if not ret:
                 break
-            out.write(frame)
-
+            if frame is not None:
+                out.write(frame)
         out.release()
         print(f"Video saved to {output_filename}")
 
-    release_rtsp_stream(cap)
+        release_rtsp_stream(cap)
+        
 
 # Example usage:
 def main():
