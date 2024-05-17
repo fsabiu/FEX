4ede47be049eaf7d443382f53ea51734b1e467da
ntefa <andrea.batino94@gmail.com>
2024-05-17 09:21:57 +0000

    updated downloadFrames to handle no streaming scenarios

--- scripts/downloadFrames.py
+++ scripts/downloadFrames.py
@@ -2,12 +2,15 @@ import cv2
 import os
 from datetime import datetime
 import sys
+import time
 
 def open_rtsp_stream(stream_url):
-    cap = cv2.VideoCapture(stream_url)
-    if not cap.isOpened():
-        print("Error: Could not open the video stream.")
-        return None
+    cap = None
+    while cap is None or not cap.isOpened():
+        cap = cv2.VideoCapture(stream_url)
+        if not cap.isOpened():
+            print("Waiting for stream ...")
+            time.sleep(1)
     return cap
 
 def release_rtsp_stream(cap):
@@ -15,50 +18,53 @@ def release_rtsp_stream(cap):
         cap.release()
         cv2.destroyAllWindows()
 
-def save_frames_from_stream(stream_url, output_folder,n):
-    cap = open_rtsp_stream(stream_url)
-    if cap is None:
-        return
-
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-    # Generate output filename with timestamp
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    output_subfolder = f"{output_folder}/frame_{timestamp}"
-    if not os.path.exists(output_subfolder):
-        os.makedirs(output_subfolder)
+def save_frames_from_stream(stream_url, output_folder, n):
     
-    # initialize variables for FPS calculation
-    num_frames = 0
-    interval = 1  # interval for FPS calculation in seconds
-    
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-        num_frames += 1
+    while True:
+        cap = open_rtsp_stream(stream_url)
+        if cap is None:
+            continue
 
-        # check if it's time to save the image
-        if num_frames % n == 0:
-            # write image to file
-            cv2.imwrite(os.path.join(output_subfolder, 'frame_{}.jpg'.format(num_frames)), frame)
-            print("Saved frame:", num_frames)
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+        
+        # Generate output filename with timestamp
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        output_subfolder = f"{output_folder}/frame_{timestamp}"
+        if not os.path.exists(output_subfolder):
+            os.makedirs(output_subfolder)
 
-    release_rtsp_stream(cap)
-    print(f"{num_frames} frames saved to {output_folder}")
+        num_frames = 0
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                print("Stream closed. Reopening...")
+                break
+            
+            num_frames += 1
+
+            # check if it's time to save the image
+            if num_frames % n == 0:
+                # write image to file
+                cv2.imwrite(os.path.join(output_subfolder, f'frame_{num_frames}.jpg'), frame)
+                print("Saved frame:", num_frames)
+
+        release_rtsp_stream(cap)
+        print(f"{num_frames} frames saved to {output_folder}")
 
 # Example usage:
 def main():
-   
-    # stream_url = 'rtsp://rtspstream:e13d6ca8e1ce8e3c913d7555c48342e4@zephyr.rtsp.stream/movie'
-    # frames_output_folder = 'frames'
 
+    if len(sys.argv) < 4:
+        print("Usage: python script.py <stream_url> <output_folder> <n>")
+        return
+    
     stream_url = sys.argv[1]
     frames_output_folder = sys.argv[2]
     n_str = sys.argv[3]
 
     if not n_str.isdigit():
-        print("Error: Delay parameter must be an integer.")
+        print("Error: n parameter must be an integer.")
         return
 
     n = int(n_str)
