3a1f281bd061d1bf13133c5dbc7ac5735cf14df6
Ubuntu <fsabiu@gmail.com>
2024-05-17 16:10:24 +0000

    orcnn utils

--- scripts/network.sh
+++ scripts/network.sh
@@ -1,4 +1,5 @@
 sudo apt -y install firewalld
 sudo firewall-cmd --zone=public --add-port=2053/tcp --permanent
 sudo firewall-cmd --zone=public --add-port=2054/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=2055/tcp --permanent
 sudo firewall-cmd --reload
