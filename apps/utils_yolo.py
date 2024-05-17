1a4d461aa1295cc70d8b22bfc0e7036acf7fea17
Ubuntu <gaplanelles@gmail.com>
2024-05-17 14:38:34 +0000

    app_yolo_tanks_trucks

--- apps/utils_yolo.py
+++ apps/utils_yolo.py
@@ -7,16 +7,17 @@ import supervision as sv
 import os
 import numpy as np
 from PIL import Image
+import torch
 import time
 #from google.colab.patches import cv2_imshow
 
 
-def load_yolow():
+def load_yolo(model):
     # Create the 'models' folder if it doesn't exist
     if not os.path.exists('models'):
         os.mkdir('models')
     
-    model = YOLO('models/yolov8l-world.pt') 
+    model = YOLO('models/'+str(model)) #set this up with the trained yolo model
 
     return model
 
@@ -26,14 +27,14 @@ def writeLog(path, obj):
         logfile.write(date_time + ":" + str(obj) + "\n")
 
 def detect_yolo(model, b64_image, confidence):
-    model.set_classes(["car", "pedestrian crossing", "train", "ship"])
+    #model.set_classes(["car", "pedestrian crossing", "train", "ship"])
     bounding_box_annotator = sv.BoundingBoxAnnotator()
     label_annotator = sv.LabelAnnotator()
 
     start_time = time.time()
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S%f")
     frmt = "png"
-    name = f"yolow_img_{timestamp}.{frmt}"
+    name = f"yolo_img_{timestamp}.{frmt}"
     path = f"images/{name}"
 
 
@@ -44,39 +45,102 @@ def detect_yolo(model, b64_image, confidence):
     else: # if b64_image is a path read it
         path = b64_image
 
-    im = Image.open(path)
-    img_array = np.asarray(im)
+    #im = Image.open(path)
+    #img_array = np.asarray(im)
 
     # Checking number of channels
-    if img_array.shape[2] == 4:
-        img_array = img_array[:, :, :3]  # Drop the last channel (alpha channel)
+    #if img_array.shape[2] == 4:
+    #    img_array = img_array[:, :, :3]  # Drop the last channel (alpha channel)
 
-    writeLog("logs_yolo.txt", "yolo - Image shape: " + str(np.shape(img_array)))
+    #writeLog("logs_yolo.txt", "yolo - Image shape: " + str(np.shape(img_array)))
     # Detection
-    result = results = model.predict(img_array)
+    results = model(path)
 
     end_time = time.time()
     elapsed_time = end_time - start_time
     writeLog("logs_yolo.txt", "yolo - Inference time: " + str(elapsed_time) + " seconds")
 
-    detections = sv.Detections.from_ultralytics(results[0])
-    annotated_frame = bounding_box_annotator.annotate(
-        scene = img_array.copy(),
-        detections = detections[detections.confidence > 0.001]
-    )
+    #detections = sv.Detections.from_ultralytics(results[0])
+    #annotated_frame = bounding_box_annotator.annotate(
+    #    scene = img_array.copy(),
+    #    detections = detections[detections.confidence > 0.001]
+    #)
     #annotated_frame = label_annotator.annotate(
     #    scene=annotated_frame, detections = detections
     #)
-    polygon_annotator = sv.PolygonAnnotator()
-    annotated_frame = polygon_annotator.annotate(
-    scene=annotated_frame,
-    detections=detections
-    )
+    #polygon_annotator = sv.PolygonAnnotator()
+    #annotated_frame = polygon_annotator.annotate(
+    #scene=annotated_frame,
+    #detections=detections
+    #)
 
     #objects = getObjects("yolow", model, result)
 
-    resDicts = {}
-    #resDicts["objects"] = objects
-    #resDicts["all_classes"] = model.CLASSES
+    img = results[0].orig_img
+    img_w, img_h = get_np_image_size(img)
 
+    boxes = [b.xywh for b in results[0].boxes]
+    confidences = [b.conf for b in results[0].boxes]
+    class_ids = [int(b[5]) for b in results[0].boxes.data]
+    class_names = [model.names[class_id] for class_id in class_ids]
+
+    objects = []
+    for i, box in enumerate(boxes):
+        obj = xywh2xiyi(box, img_w, img_h) # returns dict of bounds: x1 y1, .... x4 y4
+        obj["confidence"] = float(confidences[i])
+        obj["tagName"] = class_names[i]
+        objects.append(obj)
+
+    resDicts = {}
+    resDicts["objects"] = objects
+    resDicts["all_classes"] = class_ids
+    resDicts["time"] = elapsed_time
+    writeLog("logs_yolo.txt", resDicts)
     return resDicts
+
+
+
+def get_np_image_size(image):
+    if image.ndim == 3:  # Color image
+        height, width, channels = image.shape
+    elif image.ndim == 2:  # Grayscale image
+        height, width = image.shape
+        channels = 1  # Grayscale image has 1 channel
+    else:
+        raise ValueError("Unsupported image dimensions")
+    
+    return width, height
+
+def xywh2xiyi(xywh, img_w, img_h):
+    x1 = y1 = x2 = y2 = x3 = y3 = x4 = y4 = 0
+
+    if isinstance(xywh, torch.Tensor):
+        x_center = float(xywh[0, 0])
+        y_center = float(xywh[0, 1])
+        width = float(xywh[0, 2])
+        height = float(xywh[0, 3])
+
+        # Convert relative coordinates to absolute coordinates
+    x1 = int((x_center - width / 2) * img_w)
+    y1 = int((y_center - height / 2) * img_h)
+    x2 = int((x_center + width / 2) * img_w)
+    y2 = int((y_center - height / 2) * img_h)
+    x3 = int((x_center + width / 2) * img_w)
+    y3 = int((y_center + height / 2) * img_h)
+    x4 = int((x_center - width / 2) * img_w)
+    y4 = int((y_center + height / 2) * img_h)
+
+    bounds = {
+        "bounds": {
+            "x1": x1,
+            "y1": y1,
+            "x2": x2,
+            "y2": y2,
+            "x3": x3,
+            "y3": y3,
+            "x4": x4,
+            "y4": y4
+        }
+    }
+
+    return bounds
