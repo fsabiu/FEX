2edaa17931c45636b33d8e293ca61c267c40be5f
fsabiu <fsabiu@gmail.com>
2024-05-19 12:29:57 +0000

    streaming on

--- apps/main_detect.py
+++ apps/main_detect.py
@@ -15,6 +15,62 @@ import queue as queue_lib
 
 def annotate_img_opencv(image, annotations):
     """
+    Annotate the image with bounding boxes and labels.
+
+    Args:
+    - image: OpenCV image (numpy array)
+    - annotations: List of dictionaries containing annotation bounds and tag names
+
+    Returns:
+    - Annotated OpenCV image (numpy array)
+    """
+    # Iterate through each annotation
+    for annotation in annotations:
+        bounds = annotation["bounds"]
+        tag_name = annotation.get("tagName")
+        confidence = round(float(annotation.get("confidence")), 2)
+        text= str(tag_name)+" - "+str(confidence)
+        
+        x1 = int(bounds["x1"])
+        y1 = int(bounds["y1"])
+        x2 = int(bounds["x2"])
+        y2 = int(bounds["y2"])
+        x3 = int(bounds["x3"])
+        y3 = int(bounds["y3"])
+        x4 = int(bounds["x4"])
+        y4 = int(bounds["y4"])
+        
+        # Define the points of the polygon
+        points = np.array([[x1, y1], [x2, y2], [x3, y3], [x4, y4]], np.int32)
+        points = points.reshape((-1, 1, 2))
+        
+        # Draw the polygon on the image
+        cv2.polylines(image, [points], isClosed=True, color=(0, 0, 255), thickness=2)
+        
+        # Get the width and height of the text box
+        (text_width, text_height), baseline = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.7, thickness=2)        
+        
+        # Calculate the position for the text box
+        text_x = x1
+        text_y = y1 - 10  # slightly above the top-left corner of the bounding box
+        
+        # Ensure the text box is within the image bounds
+        text_x = max(0, text_x)
+        text_y = max(text_height + baseline, text_y)
+        
+        # Draw a filled rectangle for the text background
+        cv2.rectangle(image, (text_x, text_y - text_height - baseline), 
+                      (text_x + text_width, text_y + baseline), 
+                      (0, 0, 255), thickness=cv2.FILLED)
+        
+        # Put the text on the image
+        cv2.putText(image, text, (text_x, text_y - baseline), 
+                    cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.7, color=(255, 255, 255), thickness=2)
+    
+    return image
+
+def annotate_img_opencv_old(image, annotations):
+    """
     Annotate the image with bounding boxes.
 
     Args:
@@ -47,16 +103,35 @@ def annotate_img_opencv(image, annotations):
 
 def create_writer(rtsp_url,width,height,fps):
      # Define VideoWriter properties
-    fourcc = cv2.VideoWriter_fourcc(*'H264')  # Codec for the video stream
+    fourcc = cv2.VideoWriter_fourcc(*'X264')  # Codec for the video stream
     # out = cv2.VideoWriter(rtsp_url, fourcc, fps, (width, height))
     
     # Create VideoWriter object
         # ' !videorate max-rate=4 ' + \
     out = cv2.VideoWriter('appsrc ! videoconvert' + \
+        # ' !videorate max-rate=4 ' + \
         ' ! video/x-raw,format=I420' + \
         ' ! x264enc  speed-preset=medium tune=zerolatency bitrate=800' \
         ' ! video/x-h264,profile=baseline' + \
         ' ! rtspclientsink location=' + rtsp_url,
+        cv2.CAP_GSTREAMER, fourcc, fps, (width, height), True)
+    if not out.isOpened():
+        raise RuntimeError("Can't open video writer")
+    return out
+
+def create_writer_hls(hls_url,width,height,fps):
+     # Define VideoWriter properties
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Codec for the video stream
+    # out = cv2.VideoWriter(rtsp_url, fourcc, fps, (width, height))
+    
+    # Create VideoWriter object
+        # ' !videorate max-rate=4 ' + \
+    out = cv2.VideoWriter('appsrc ! videoconvert' + \
+        # ' !videorate max-rate=4 ' + \
+        ' ! video/x-raw,format=I420' + \
+        ' ! x264enc  speed-preset=slow tune=zerolatency bitrate=5000' \
+        ' ! mpegtsmux' + \
+        ' ! hlssink location=' + hls_url,
         cv2.CAP_GSTREAMER, fourcc, fps, (width, height), True)
     if not out.isOpened():
         raise RuntimeError("Can't open video writer")
@@ -144,6 +219,7 @@ def save_image_with_timestamp(image):
 def consumer(queue_in, to_print=False, confidence_filters=None):
     try:
         rtsp_url = "rtsp://localhost:8554/mystream"
+        hls_url = "rtsp://localhost:8888/mystream"
         fps = 30
         out = None
         out_w = None
@@ -154,23 +230,24 @@ def consumer(queue_in, to_print=False, confidence_filters=None):
         init_frame = 50
 
         # TODO REMOVE
-        out_w, out_h = 300, 300
-        #out = create_writer(rtsp_url, out_w, out_h, fps)
+        # out_w, out_h = 300, 300
+        # out = create_writer(rtsp_url, out_w, out_h, fps)
 
         while True:
             processed_frame = queue_in.get()
             frame = processed_frame["frame"]
             res_dicts = processed_frame["res_dicts"]
             frame_h, frame_w = frame.shape[:2]
-            #if out is None or frame_h != out_h or frame_w != out_w:
-            #    out_h, out_w = frame_h, frame_w
-            #    out = create_writer(rtsp_url, frame_w, frame_h, fps)
+            if out is None or frame_h != out_h or frame_w != out_w:
+               out_h, out_w = frame_h, frame_w
+               out = create_writer(rtsp_url, frame_w, frame_h, fps)
+               out_hls = create_writer_hls(hls_url, frame_w, frame_h, fps)
                 
             filtered_objects = utils_yolo.get_filtered_objects(res_dicts, confidence_filters)
             annotated_frame = annotate_img_opencv(frame, filtered_objects)
-            #out.write(annotated_frame)
+            out.write(annotated_frame)
             print(np.shape(annotated_frame))
-            save_image_with_timestamp(annotated_frame)
+            # save_image_with_timestamp(annotated_frame)
 
     except KeyboardInterrupt:
         pass
@@ -183,7 +260,7 @@ if __name__ == "__main__":
     Usage: demo_multiprocess.py stream_url 
     """
 
-    n_producers = 2
+    n_producers = 1
 
     queue_read2aggr = Queue()
     queues_read2pred = [Queue() for _ in range(n_producers)]
@@ -198,11 +275,11 @@ if __name__ == "__main__":
     processes.append(read_frames_process)
 
     yolo_paths = [
-        "../apps/models/best_tanks_militaryTrucks.pt",
+        "../apps/models/yolo_VDTMLT_620p.pt",
         "../apps/models/best_tanks_militaryTrucks.pt",
     ]
     yolo_confidence_filters = [
-        dict(military_tank=0.05, military_truck=0.05),
+        dict(pedestrian=0.3, car=0.3, van=0.3, truck=0.3, military_tank=0.3, military_truck=0.3, military_vehicle=0.3),
         dict(military_tank=0.05, military_truck=0.05),
     ]
     
