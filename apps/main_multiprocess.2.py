357221cb436bc66101ffb53b7af75e93fc63e871
fsabiu <fsabiu@gmail.com>
2024-05-19 09:05:20 +0000

    streaming

--- apps/main_multiprocess.2.py
+++ apps/main_multiprocess.2.py
@@ -45,22 +45,17 @@ def annotate_img_opencv(image, annotations):
 
 def create_writer(rtsp_url,width,height,fps):
      # Define VideoWriter properties
-    fourcc = cv2.VideoWriter_fourcc(*'MJPG')  # Codec for the video stream
-    # fps = 20.0
-    # frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    # frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-    # OpenCV VideoWriter to RTSP server
-    out = cv2.VideoWriter(rtsp_url, fourcc, fps, (width, height))
+    fourcc = cv2.VideoWriter_fourcc(*'H264')  # Codec for the video stream
+    # out = cv2.VideoWriter(rtsp_url, fourcc, fps, (width, height))
     
     # Create VideoWriter object
         # ' !videorate max-rate=4 ' + \
-    # out = cv2.VideoWriter('appsrc ! videoconvert' + \
-    #     ' ! video/x-raw,format=I420' + \
-    #     ' ! x264enc  speed-preset=medium tune=zerolatency bitrate=800' \
-    #     ' ! video/x-h264,profile=baseline' + \
-    #     ' ! rtspclientsink location=' + rtsp_url,
-        # cv2.CAP_GSTREAMER, fourcc, fps, (width, height), True)
+    out = cv2.VideoWriter('appsrc ! videoconvert' + \
+        ' ! video/x-raw,format=I420' + \
+        ' ! x264enc  speed-preset=medium tune=zerolatency bitrate=800' \
+        ' ! video/x-h264,profile=baseline' + \
+        ' ! rtspclientsink location=' + rtsp_url,
+        cv2.CAP_GSTREAMER, fourcc, fps, (width, height), True)
     if not out.isOpened():
         raise RuntimeError("Can't open video writer")
     return out
@@ -85,6 +80,8 @@ def read_frames(stream_url, aggr_queue, queues, to_print=False):
                 print("fps_so_far:", 1/elapsed)
     except KeyboardInterrupt:
         pass
+    except RuntimeError as E:
+        print(E)
     print("read_frames", "completed")
 
 def predictor(model_path, queue_in, queue_out, p_id):
@@ -100,6 +97,8 @@ def predictor(model_path, queue_in, queue_out, p_id):
             queue_out.put({"yolo_producer": p_id, "res_dict": res_dict})
     except KeyboardInterrupt:
         pass
+    except RuntimeError as E:
+        print(E)
     print("predictor", p_id, "completed")
 
 def aggregator(frame_queue, queues_in, queue_out, confidence_filters=None):
@@ -120,6 +119,8 @@ def aggregator(frame_queue, queues_in, queue_out, confidence_filters=None):
             })
     except KeyboardInterrupt:
         pass
+    except RuntimeError as E:
+        print(E)
     print("aggregator", "completed")
 
 def consumer(queue_in, to_print=False, confidence_filters=None):
@@ -133,6 +134,11 @@ def consumer(queue_in, to_print=False, confidence_filters=None):
             print("consumer is printing")
         frames = 0
         init_frame = 50
+
+        # TODO REMOVE
+        out_w, out_h = 300, 300
+        out = create_writer(rtsp_url, out_w, out_h, fps)
+
         while True:
             processed_frame = queue_in.get()
             frame = processed_frame["frame"]
@@ -158,6 +164,8 @@ def consumer(queue_in, to_print=False, confidence_filters=None):
                 print("consumer: ", (frames-init_frame)/elapsed)
     except KeyboardInterrupt:
         pass
+    except RuntimeError as E:
+        print(E)
     print("consumer", "completed")
 
 if __name__ == "__main__":
@@ -165,70 +173,73 @@ if __name__ == "__main__":
     Usage: demo_multiprocess.py stream_url 
     """
 
-    n_producers = 2
+    # n_producers = 2
 
-    # yolo1_confidence_filters = {
-    #     "military_tank": 0.05,
-    #     "military_truck": 0.05
-    # }
+    # # yolo1_confidence_filters = {
+    # #     "military_tank": 0.05,
+    # #     "military_truck": 0.05
+    # # }
 
-    # yolo2_confidence_filters = {
-    #     "military_tank": 0.05,
-    #     "military_truck": 0.05
-    # }
+    # # yolo2_confidence_filters = {
+    # #     "military_tank": 0.05,
+    # #     "military_truck": 0.05
+    # # }
 
-    queue_read2aggr = Queue()
-    queues_read2pred = [Queue() for _ in range(n_producers)]
-    queues_pred2aggr = [Queue() for _ in range(n_producers)]
-    queue_aggr2cons = Queue()
+    # queue_read2aggr = Queue()
+    # queues_read2pred = [Queue() for _ in range(n_producers)]
+    # queues_pred2aggr = [Queue() for _ in range(n_producers)]
+    # queue_aggr2cons = Queue()
 
-    queues = [queue_read2aggr, queue_aggr2cons] + queues_read2pred + queues_pred2aggr
-    processes = []
+    # queues = [queue_read2aggr, queue_aggr2cons] + queues_read2pred + queues_pred2aggr
+    # processes = []
 
-    stream_url = sys.argv[1]
-    read_frames_process = Process(target=read_frames, args=(stream_url, queue_read2aggr, queues_read2pred))
-    processes.append(read_frames_process)
+    # stream_url = sys.argv[1]
+    # read_frames_process = Process(target=read_frames, args=(stream_url, queue_read2aggr, queues_read2pred))
+    # processes.append(read_frames_process)
 
-    yolo_paths = [
-        "../apps/models/best_tanks_militaryTrucks.pt",
-        "../apps/models/best_tanks_militaryTrucks.pt",
-    ]
-    yolo_confidence_filters = [
-        dict(military_tank=0.05, military_truck=0.05),
-        dict(military_tank=0.05, military_truck=0.05),
-    ]
+    # yolo_paths = [
+    #     "../apps/models/best_tanks_militaryTrucks.pt",
+    #     "../apps/models/best_tanks_militaryTrucks.pt",
+    # ]
+    # yolo_confidence_filters = [
+    #     dict(military_tank=0.05, military_truck=0.05),
+    #     dict(military_tank=0.05, military_truck=0.05),
+    # ]
     
-    predictor_procesesses = [
-        Process(
-            target=predictor,
-            args=(yolo_paths[i], queues_read2pred[i], queues_pred2aggr[i], i)
-        ) for i in range(n_producers)
-    ]
-    processes += predictor_procesesses
+    # predictor_procesesses = [
+    #     Process(
+    #         target=predictor,
+    #         args=(yolo_paths[i], queues_read2pred[i], queues_pred2aggr[i], i)
+    #     ) for i in range(n_producers)
+    # ]
+    # processes += predictor_procesesses
 
-    aggregator_process = Process(target=aggregator,
-        args=(queue_read2aggr, queues_pred2aggr, queue_aggr2cons))
-    processes.append(aggregator_process)
+    # aggregator_process = Process(target=aggregator,
+    #     args=(queue_read2aggr, queues_pred2aggr, queue_aggr2cons))
+    # processes.append(aggregator_process)
 
-    consumer_process = Process(target=consumer, 
-        args=(queue_aggr2cons, False, yolo_confidence_filters))
-    processes.append(consumer_process)
+    # consumer_process = Process(target=consumer, 
+    #     args=(queue_aggr2cons, False, yolo_confidence_filters))
+    # processes.append(consumer_process)
 
-    for process in processes:
-        process.start()
+    # for process in processes:
+    #     process.start()
 
-    try:
-        for process in processes:
-            process.join()
-    except RuntimeError:
-        pass
-    print("main", "completed")
+    # try:
+    #     for process in processes:
+    #         process.join()
+    # except KeyboardInterrupt:
+    #     pass
+    # except RuntimeError as E:
+    #     print(E)
+    # print("main", "completed")
 
 
-    # # Check if there were any exceptions
-    # if not error_queue.empty():
-    #     #print("An error occurred in the consumer process:")
-    #     with open('logs_ww.txt', 'a') as f:
-    #         f.write("An error occurred in the consumer process:\n")
-    #         f.write(error_queue.get())
-    #         f.write("\n")  # Ensure a new line after the traceback
+    # # # Check if there were any exceptions
+    # # if not error_queue.empty():
+    # #     #print("An error occurred in the consumer process:")
+    # #     with open('logs_ww.txt', 'a') as f:
+    # #         f.write("An error occurred in the consumer process:\n")
+    # #         f.write(error_queue.get())
+    # #         f.write("\n")  # Ensure a new line after the traceback
+    create_writer("rtsp://localhost:8554/mystream",1280,720,30)
